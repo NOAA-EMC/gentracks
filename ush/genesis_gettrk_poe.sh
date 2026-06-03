e6e9c9137fef3fa2a65dd414c146bc3d68be692e
Hana <148084243+HananehJafary-NOAA@users.noreply.github.com>
2026-06-03 17:38:25 -0400

    Revert "Adjusting ush script for the ECMWF file change (#11)"
    This reverts commit 6756670e7e92df51d69ce98515e65e7bd371eb43.

--- ush/genesis_gettrk_poe.sh
+++ ush/genesis_gettrk_poe.sh
@@ -2015,11 +2015,8 @@ if [ ${model} -eq 4 ] ; then
       let fhr=ict*6
       echo "fhr= $fhr  fhour= $fhour"
       fmmddhh=` ${NDATE} ${fhour} ${PDY}${CYL} | cut -c5- `
-      if [ "${fhr}" -eq 0 ]; then
-        ec_hires_orig=DCD${immddhh}00${fmmddhh}01${ECMWF_FILE_EXT:-1}
-      else
-        ec_hires_orig=DCD${immddhh}00${fmmddhh}00${ECMWF_FILE_EXT:-1}
-      fi
+      ec_hires_orig=DCD${immddhh}00${fmmddhh}00${ECMWF_FILE_EXT:-1}
+#      ec_hires_orig=ecens_DCD${immddhh}00${fmmddhh}001
 
       total_file_cnt=$(($total_file_cnt+1))
       if [ ! -s ${ecmwfdir}/${ec_hires_orig} ]
